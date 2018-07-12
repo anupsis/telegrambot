eecde4ee6f623e7213c9c69ba846b2ca170dae83
Efrem Heri Budiarto <ediarto@gmail.com>
2018-07-12 08:06:06 +0700

    test hook

--- hook.php
+++ hook.php
@@ -10,20 +10,38 @@ $bot_username = $BOT_NAME;
 try {
     // Create Telegram API object
     $telegram = new Longman\TelegramBot\Telegram($bot_api_key, $bot_username);
-
+    // Add commands paths containing your custom commands
+    $telegram->addCommandsPaths($commands_paths);
+    // Enable admin users
+    $telegram->enableAdmins($admin_users);
+    // Enable MySQL
+    //$telegram->enableMySql($mysql_credentials);
+    // Logging (Error, Debug and Raw Updates)
+    Longman\TelegramBot\TelegramLog::initErrorLog(__DIR__ . "/{$bot_username}_error.log");
+    Longman\TelegramBot\TelegramLog::initDebugLog(__DIR__ . "/{$bot_username}_debug.log");
+    Longman\TelegramBot\TelegramLog::initUpdateLog(__DIR__ . "/{$bot_username}_update.log");
+    // If you are using a custom Monolog instance for logging, use this instead of the above
+    //Longman\TelegramBot\TelegramLog::initialize($your_external_monolog_instance);
+    // Set custom Upload and Download paths
+    //$telegram->setDownloadPath(__DIR__ . '/Download');
+    //$telegram->setUploadPath(__DIR__ . '/Upload');
+    // Here you can set some command specific parameters
+    // e.g. Google geocode/timezone api key for /date command
+    //$telegram->setCommandConfig('date', ['google_api_key' => 'your_google_api_key_here']);
+    // Botan.io integration
+    //$telegram->enableBotan('your_botan_token');
+    // Requests Limiter (tries to prevent reaching Telegram API limits)
+    $telegram->enableLimiter();
     // Handle telegram webhook request
     $telegram->handle();
 } catch (Longman\TelegramBot\Exception\TelegramException $e) {
     // Silence is golden!
-    // log telegram errors
-	echo "<pre>";
-	print_r($e);
-	$myfile = fopen("newfile.txt", "w") or die("Unable to open file!");
-	$txt = "John Doe\n";
-	fwrite($myfile, $e);
-	// $txt = "Jane Doe\n";
-	// fwrite($myfile, $txt);
-	fclose($myfile);
-    echo $e->getMessage();
+    //echo $e;
+    // Log telegram errors
+    Longman\TelegramBot\TelegramLog::error($e);
+} catch (Longman\TelegramBot\Exception\TelegramLogException $e) {
+    // Silence is golden!
+    // Uncomment this to catch log initialisation errors
+    //echo $e;
 }
 ?>
